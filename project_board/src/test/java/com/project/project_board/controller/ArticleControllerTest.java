d7421f5a55b06e32ed9fc537f61cfce77af3aa9a
NAM <hbkt1857@gmail.com>
2024-01-10 00:15:20 +0900

    #5 - 게시글 관련 뷰 테스트 작성
    테스트 뷰 경로 설정

---
+++ project_board/src/test/java/com/project/project_board/controller/ArticleControllerTest.java
@@ -0,0 +1,68 @@
+package com.project.project_board.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    @Test
+    public void RequestingArticlesView_ReturnsArticlesView() throws Exception {
+        //Given
+
+        //When + then
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles")); // 해당 이름을 가진 속성이 있는 지 확인
+    }
+
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
+    @Test
+    public void RequestingArticleView_ReturnsArticleView() throws Exception {
+        //Given
+
+        //When + then
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article")); // 해당 이름을 가진 속성이 있는 지 확인
+    }
+
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    public void RequestingArticleSearchView_ReturnsArticleSearchView() throws Exception {
+        //Given
+
+        //When + then
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    public void RequestingArticleHashtagSearchView_ReturnsArticleHashtagSearchView() throws Exception {
+        //Given
+        //When + then
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+}
